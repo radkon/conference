82a1ac4fbd0b45962db2e52a6da44770f7b92c1f
Markus Konrad <markus.konrad@prodyna.com>
2013-10-23 21:31:51 +0200

    LoggingInterceptor logs class, method and parameters

--- src/main/java/com/prodyna/pac/conference/core/LoggingInterceptor.java
+++ src/main/java/com/prodyna/pac/conference/core/LoggingInterceptor.java
@@ -3,6 +3,7 @@ package com.prodyna.pac.conference.core;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
 @Logged
@@ -13,13 +14,26 @@ public class LoggingInterceptor {
 
     @AroundInvoke
     public Object intercept(InvocationContext context) throws Exception {
-        String methodName = context.getMethod().getName();
-        logger.info("ENTER: " + methodName);
-        try {
-            return context.proceed();
-        } finally {
-            logger.info("LEAVE: " + methodName);
+        final Method method = context.getMethod();
+        final String className = method.getDeclaringClass().getName();
+        final String methodName = context.getMethod().getName();
+        final String parameterList = buildParameterList(context);
+        logger.info(">>> " + className + "." + methodName + parameterList);
+        final Object result = context.proceed();
+        logger.info("<<< " + className + "." + methodName + parameterList);
+        return result;
+    }
+
+    private String buildParameterList(InvocationContext context) {
+        final StringBuilder parameterListBuilder = new StringBuilder("(");
+        final Object[] parameters = context.getParameters();
+        for (int i = 0, length = parameters.length; i < length; i++) {
+            parameterListBuilder.append(parameters[i].toString());
+            if (length > 1 && i < length) {
+                parameterListBuilder.append(", ");
+            }
         }
+        return parameterListBuilder.append(")").toString();
     }
 
 }
